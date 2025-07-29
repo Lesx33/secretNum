c8ce242d53ed0d3dd65dff34bf2930490185d518
Lesx33 <fernandez.leslya@gmail.com>
2025-07-29 14:39:50 -0600

    Numero secreto con loop

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt('Indica un numero: ');
+//Variables
+let numeroSecreto = 2;
+let numeroUsuario;
+let intentos = 1;
+while (numeroSecreto != numeroUsuario) {
+     numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
+     console.log(numeroUsuario);
+     /*
+     Este código realiza
+     la comparación
+     */
+     if (numeroUsuario == numeroSecreto) {
+          //Acertamos, fue verdadera la condición
+          alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} intento(s)`);
+     } else {
+          //La condición no se cumplió
+          if (numeroUsuario > numeroSecreto) {
+               alert('El número secreto es menor');
+          } else {
+               alert('El número secreto es mayor');
+          }
+          intentos++;
 
-if (numeroSecreto == numeroUsuario){
-     alert('Acertaste el num')
+     }
 }
